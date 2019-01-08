f3d2c55f7e0d85581e594306d33eefe1ed86d406
TheCooler <truecooler@gmail.com>
2019-01-08 21:23:11 +0300

    убрал старый мусорный проект

--- VkBotFramework.Examples/MessagesWithKeyboard/Program.cs
+++ VkBotFramework.Examples/MessagesWithKeyboard/Program.cs
@@ -9,74 +9,76 @@ using VkNet.Model.RequestParams;
 
 namespace MessagesWithKeyboard
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-	        ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
-	        ILogger<VkBot> logger = loggerFactory.CreateLogger<VkBot>();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
+			ILogger<VkBot> logger = loggerFactory.CreateLogger<VkBot>();
 
-	        ExampleSettings settings = ExampleSettings.TryToLoad(logger);
+			ExampleSettings settings = ExampleSettings.TryToLoad(logger);
 
-	        VkBot bot = new VkBot(settings.AccessToken, settings.GroupUrl, logger);
+			VkBot bot = new VkBot(settings.AccessToken, settings.GroupUrl, logger);
 
-	        var isBotStarted = false;
+			var isBotStarted = false;
 
-	        int validAnswer = 0;
+			int validAnswer = 0;
 
-	        
-			bot.TemplateManager.Register("матеша", (sender,eventArgs) =>
-	        {
-		        isBotStarted = true;
+
+			bot.TemplateManager.Register("матеша", (sender, eventArgs) =>
+			{
+				isBotStarted = true;
 				var rand = new Random();
-		        var firstNum = rand.Next(1, 100);
-		        var secondNum = rand.Next(1, 100);
+				var firstNum = rand.Next(1, 100);
+				var secondNum = rand.Next(1, 100);
 
-		        validAnswer = firstNum + secondNum;
+				validAnswer = firstNum + secondNum;
 
-		        int buttonsCount = 4;
+				int buttonsCount = 4;
 
 				int validButtonIndex = rand.Next(buttonsCount);
-		        var keyboard = new KeyboardBuilder();
-		        for (int i = 0; i < buttonsCount; i++)
-		        {
+				var keyboard = new KeyboardBuilder();
+				for (int i = 0; i < buttonsCount; i++)
+				{
 					if (i == validButtonIndex)
 						keyboard.AddButton((validAnswer).ToString(), i.ToString());
 					else
 						keyboard.AddButton(rand.Next(200).ToString(), i.ToString());
 				}
 
-		        sender.Api.Messages.Send(new MessagesSendParams()
-		        {
-			        RandomId = Environment.TickCount,
-			        PeerId = eventArgs.PeerId,
-					Message = $"сколько будет {firstNum} + {secondNum}?",
-					Keyboard = keyboard.Build()
-					
-		        });
-	        });
-
-			bot.TemplateManager.Register("матеша", "ну окей, вот тебе матеша");
-
-			bot.TemplateManager.Register(@"\d+$", (sender,eventArgs) =>
-			{
-				sender.Logger.LogInformation(eventArgs.Text);
-				if (!isBotStarted) return;
-				int userAnswer = int.Parse(Regex.Match(eventArgs.Text,@"\d+$").Value);
-				var keyboard = new KeyboardBuilder().AddButton("матеша","1");
 				sender.Api.Messages.Send(new MessagesSendParams()
 				{
 					RandomId = Environment.TickCount,
 					PeerId = eventArgs.PeerId,
-					Message = (userAnswer == validAnswer )?"верный ответ!" : $"неверно! верный ответ был: {validAnswer}, попробуйте еще раз",
+					Message = $"сколько будет {firstNum} + {secondNum}?",
+					Keyboard = keyboard.Build()
+
+				});
+			});
+
+			bot.TemplateManager.Register("матеша", "ну окей, вот тебе матеша");
+
+			bot.TemplateManager.Register(@"\d+$", (sender, eventArgs) =>
+			{
+				sender.Logger.LogInformation(eventArgs.Text);
+				if (!isBotStarted) return;
+				int userAnswer = int.Parse(Regex.Match(eventArgs.Text, @"\d+$").Value);
+				var keyboard = new KeyboardBuilder().AddButton("матеша", "1");
+				sender.Api.Messages.Send(new MessagesSendParams()
+				{
+					RandomId = Environment.TickCount,
+					PeerId = eventArgs.PeerId,
+					Message = (userAnswer == validAnswer)
+						? "верный ответ!"
+						: $"неверно! верный ответ был: {validAnswer}, попробуйте еще раз",
 					Keyboard = keyboard.Build()
 
 				});
 				isBotStarted = false;
 			});
 			bot.Start();
-	        bot.Dispose();
-	        Console.ReadLine();
+			bot.Dispose();
+			Console.ReadLine();
 		}
-    }
+	}
 }
